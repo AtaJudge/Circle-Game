8ef10de532a2499c54368ddd19c5acf5eb2551ba
Ata B. YARGIÇ <yargic.ata@metu.edu.tr>
2024-05-11 20:27:43 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,3 +1,8 @@
+/*
+ * Ata Berke Yargıç - 2638328
+ * CNG 140 Assignment-2 (Circle Game)
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -11,6 +16,8 @@
 int stepPlayer(char[BOARD_SIZE][BOARD_SIZE], int, int, int, int, int, int);
 
 int stepComputer(char[BOARD_SIZE][BOARD_SIZE], int, int, int, int, int, int);
+
+int move(char[BOARD_SIZE][BOARD_SIZE], int, int, int, int, int, int, char );
 
 int checkTrapIndex(int, int);
 
@@ -26,28 +33,30 @@ int checkIsOnTrap(int, int, int, int, int);
 
 
 int main() {
-    srand(time(NULL));
-
     int row = BOARD_SIZE, col = BOARD_SIZE; // Dimensions of the game board.
     int dice1, dice2;
-    int start = 2;  // Player starts if 0, computer starts if 1, default = 2;
     int quit = 0;   // Default value is 0, if user don't want to play again it is 1.
-    int *ct_row, *ct_col; // Row and col values for computer trap index.
-    int p_trap[2], c_trap[2], t_val[2];  /* player trap index (row col),
-                                          * computer trap index (row col),
-                                          * trap val (0 = player, 1 = computer)
-                                          */
+    int *c_trap_row, *c_trap_col; // Row and col values for computer trap index.
+    int p_trap[2], c_trap[2], p_trap_val, c_trap_val;  /* player trap index (row col),
+                                                  * computer trap index (row col),
+                                                  * player's trap val.
+                                                  * computer's trap val.
+                                                  */
+
     char p_board[BOARD_SIZE][BOARD_SIZE]; // Player's board.
     char c_board[BOARD_SIZE][BOARD_SIZE]; // Computer's board.
-    char yn; // Answer of the Y/N question at the end.
+    char y_n; // Answer of the Y/N question at the end.
 
-    ct_row = &c_trap[0];
-    ct_col = &c_trap[1];
+    c_trap_row = &c_trap[0];
+    c_trap_col = &c_trap[1];
 
     printf("\nWelcome to the Circle Game! :)");
     printf("\nLet's get started!\n");
 
     while (quit == 0) {     // Continue to play until user don't want to play anymore.
+        srand(time(NULL)); // Set randomness seed of the dice according to the time.
+
+        int start = 2;  // Player starts if 0, computer starts if 1, default = 2;
         int is_over = 0;    // Default value is 0, when one of the players won it is 1.
         int p_win = 0, c_win = 0;   // Variable to determine if player and user completed a circle.
 
@@ -61,10 +70,10 @@ int main() {
         p_board[0][0] = 'P'; // Starting point for player.
         c_board[9][9] = 'C'; // Starting point for computer.
 
-        generateTrapIndex(ct_row, ct_col); // Generate trap index for computer.
-        generateTrapValues(&t_val[1], &t_val[0]); // Generate trap values randomly for all players.
-        c_trap[0] = *ct_row;
-        c_trap[1] = *ct_col;
+        generateTrapIndex(c_trap_row, c_trap_col); // Generate trap index for computer.
+        generateTrapValues(&c_trap_val, &p_trap_val); // Generate trap values randomly for all players.
+        c_trap[0] = *c_trap_row;
+        c_trap[1] = *c_trap_col;
 
         printf("\n----Player----");
         int a = 0; // It will be 1 when user enters a valid trap index.
@@ -75,17 +84,17 @@ int main() {
 
             a = checkTrapIndex(p_trap[0], p_trap[1]); // Check if index is valid.
         }
-        printf("Generated trap value: %d", t_val[0]);
+        printf("Generated trap value: %d", p_trap_val);
 
         printf("\n----Computer----");
         printf("\nGenerated trap index (row col): %d %d", c_trap[0], c_trap[1]);
-        printf("\nGenerated trap value: %d", t_val[1]);
+        printf("\nGenerated trap value: %d", c_trap_val);
 
         while (start == 2) { // Roll the dice until someone rolls bigger.
             dice1 = rollDice();
             dice2 = rollDice();
-            printf("\n\nI have rolled the dice and got %d!", dice1);
-            printf("\nI have rolled the dice for you and you got %d!", dice2);
+            printf("\n\nI have rolled the dice and got %d!", dice2);
+            printf("\nI have rolled the dice for you and you got %d!", dice1);
 
             if (dice1 > dice2)
                 start = 0; // Player starts.
@@ -98,9 +107,9 @@ int main() {
         // Display initial boards at the beginning.
         printf("\n\n----Initial boards----");
         printf("\n           Player Board");
-        displayBoard(p_board, row, col);
+        displayBoard(p_board, row, col); // Display player's board.
         printf("\n          Computer Board");
-        displayBoard(c_board, row, col);
+        displayBoard(c_board, row, col); // Display computer's board.
 
         int round = 1;
         switch (start) {
@@ -108,23 +117,28 @@ int main() {
                 while (is_over == 0) { // Play until someone completes the circle.
                     printf("\n----Round %d----", round);
 
+                    dice1 = rollDice(); // Roll the dice for Player.
+                    printf("\nI have rolled the dice for you and you got %d!", dice1);
+
                     // Player's turn.
-                        dice2 = rollDice();
-                        printf("\nI have rolled the dice for you and you got %d!", dice2);
-                        p_win = stepPlayer(p_board, row, col, dice2, p_trap[0], p_trap[1], t_val[0]);
-                        if (p_win == 1) {
-                            is_over = 1;
-                        }
+                    p_win = stepPlayer(p_board, row, col,
+                                      dice1, p_trap[0],
+                                      p_trap[1], p_trap_val);
+                    if (p_win == 1) {
+                        is_over = 1;
+                    }
+
+                    dice2 = rollDice(); // Roll the dice for Computer.
+                    printf("\nI have rolled the dice and got %d!", dice2);
 
                     // Computer's turn.
-                    if (is_over == 0) {
-                        dice1 = rollDice();
-                        printf("\nI have rolled the dice and got %d!", dice1);
-                        c_win = stepComputer(c_board, row, col, dice1, c_trap[0], c_trap[1], t_val[1]);
-                        if (c_win == 1) {
-                            is_over = 1;
-                        }
+                    c_win = stepComputer(c_board, row, col,
+                                        dice2, c_trap[0],
+                                        c_trap[1], c_trap_val);
+                    if (c_win == 1) {
+                        is_over = 1;
                     }
+
 
                     printf("\n\n           Player Board");
                     displayBoard(p_board, row, col); // Display player's board.
@@ -139,29 +153,34 @@ int main() {
                 while (is_over == 0) { // Play until someone completes the circle.
                     printf("\n----Round %d----", round);
 
+                    dice1 = rollDice(); // Roll the dice for Computer.
+                    printf("\nI have rolled the dice and got %d!", dice1);
+
                     // Computer's turn.
-                        dice1 = rollDice();
-                        printf("\nI have rolled the dice and got %d!", dice1);
-                        c_win = stepComputer(c_board, row, col, dice1, c_trap[0], c_trap[1], t_val[1]);
-                        if (c_win == 1) {
-                            is_over = 1;
-                        }
+                    c_win = stepComputer(c_board, row, col,
+                                        dice1, c_trap[0],
+                                        c_trap[1], c_trap_val);
+                    if (c_win == 1) {
+                        is_over = 1;
+                    }
+
+                    dice2 = rollDice(); // Roll the dice for Player.
+                    printf("\nI have rolled the dice for you and you got %d!", dice2);
 
                     // Player's turn.
-                    if (is_over == 0) {
-                        dice2 = rollDice();
-                        printf("\nI have rolled the dice for you and you got %d!", dice2);
-                        p_win = stepPlayer(p_board, row, col, dice2, p_trap[0], p_trap[1], t_val[0]);
-                        if (p_win == 1) {
-                            is_over = 1;
-                        }
+                    p_win = stepPlayer(p_board, row, col,
+                                      dice2, p_trap[0],
+                                      p_trap[1], p_trap_val);
+                    if (p_win == 1) {
+                        is_over = 1;
                     }
+
+
+                    printf("\n\n           Player Board");
+                    displayBoard(p_board, row, col); // Display player's board.
 
                     printf("\n          Computer Board");
                     displayBoard(c_board, row, col); // Display computer's board.
-
-                    printf("\n           Player Board");
-                    displayBoard(p_board, row, col); // Display player's board.
 
                     round++;
                 }
@@ -169,20 +188,22 @@ int main() {
         }
 
         printf("\n----END----");
-        if (p_win == 1) // Player won.
-            printf("\nYou won!!!");
+        if (p_win == 1 && c_win == 1) // Tie.
+            printf("\nTie!!!");
         else if (c_win == 1) // Computer won.
             printf("\nI won!!!");
-        else {
-            printf("\nTie!!!");
+        else if (p_win == 1) { // Player won.
+            printf("\nYou won!!!");
         }
 
         // Ask if player wants to play again.
         printf("\n\nWould you like to play again (Y/N)?");
-        scanf(" %c", &yn);
-        if (yn == 'Y' || yn == 'y')
+        scanf(" %c", &y_n);
+        fflush(stdin);
+
+        if (y_n == 'Y' || y_n == 'y')
             quit = 0;
-        else if (yn == 'N' || yn == 'n')
+        else if (y_n == 'N' || y_n == 'n')
             quit = 1;
     }
 
@@ -191,8 +212,13 @@ int main() {
 
 
 //Function to move player. If one circle completed return 1, else return 0.
-int stepPlayer(char board[BOARD_SIZE][BOARD_SIZE], int row, int col, int step, int t_row, int t_col, int t_val) {
-
+int stepPlayer(char board[BOARD_SIZE][BOARD_SIZE], int row, int col, int step, int t_row, int t_col, int trap_val) {
+    /* To make a move, the sides of the game board are first scanned to find the current position.
+     * Then check whether the move is landing on the trap index. If the move ends at the trap index,
+     * the number of steps is recalculated according to the trap value (new step = step - trap value).
+     * If the new step count is 0 or less than 0, no move is made. If it is greater than 0,
+     * move clockwise along the sides until the number of remaining step is 0.
+     */
 
     int is_trap;      // Default value is 0, it will be 1 if movement ended on a trap.
     int old_step;     // Save old step value to print it after calculated new step val.
@@ -254,81 +280,29 @@ int stepPlayer(char board[BOARD_SIZE][BOARD_SIZE], int row, int col, int step, i
     is_trap = checkIsOnTrap(step, p_row, p_col, t_row, t_col);
     if (is_trap == 1) {
         old_step = step;
-        step = step - t_val;
+        step = step - trap_val;
+
         if (step < 0)
             step = 0;
-        printf("\nTrapped at index %d %d! %d forward - %d backward = %d step.", t_row, t_col, old_step, t_val, step);
+
+        printf("\nTrapped at index %d %d! %d forward - %d backward = %d step.",
+               t_row, t_col, old_step, trap_val, step);
     }
 
-    // Move according to the direction value.
-    switch (dir) {
-        case 0: // Move to the right. If necessary down.
-            for (int i = 0; i < (col - 1); i++) {
-                if (step > 0 && p_col < (col - 1)) {
-                    step--;
-                    p_col++;
-                    board[0][p_col] = PLAYER;
-                }
-            }
-            for (int i = 0; i < (row - 1); i++) {
-                if (step > 0 && p_row < (row - 1)) {
-                    step--;
-                    p_row++;
-                    board[p_row][9] = PLAYER;
-                }
-            }
-            break;
-        case 1: // Move to the down. If necessary left.
-            for (int i = 0; i < (row - 1); i++) {
-                if (step > 0 && p_row < (row - 1)) {
-                    step--;
-                    p_row++;
-                    board[p_row][9] = PLAYER;
-                }
-            }
-            for (int i = (col - 1); i > 0; i--) {
-                if (step > 0 && p_col > 0) {
-                    step--;
-                    p_row++;
-                    board[p_row][9] = PLAYER;
-                }
-            }
-            break;
-        case 2: // Move to the left. If necessary up.
-            for (int i = (col - 1); i > 0; i--) {
-                if (step > 0 && p_col > 0) {
-                    step--;
-                    p_col--;
-                    board[9][p_col] = PLAYER;
-                }
-            }
-            for (int i = (row - 1); i > 0; i--) {
-                if (step > 0 && p_row > 0) {
-                    step--;
-                    p_row--;
-                    board[p_row][0] = PLAYER;
-                }
-            }
-            break;
-        case 3: // Move to the up.
-            for (int i = (row - 1); i > 0; i--) {
-                if (step > 0 && p_row > 0) {
-                    step--;
-                    p_row--;
-                    board[p_row][0] = PLAYER;
-                    if (p_row == 0)
-                        return 1;
-                }
-            }
-    }
 
-    return 0;
+    // Move the player. If circle completed, return 1. Else 0.
+    return move(board, dir, step, row, col, p_row, p_col, PLAYER);
 }
 
 
 //Function to move computer. If one circle completed return 1, else return 0.
-int stepComputer(char board[BOARD_SIZE][BOARD_SIZE], int row, int col, int step, int t_row, int t_col, int t_val) {
-
+int stepComputer(char board[BOARD_SIZE][BOARD_SIZE], int row, int col, int step, int t_row, int t_col, int trap_val) {
+    /* To make a move, the sides of the game board are first scanned to find the current position.
+     * Then check whether the move is landing on the trap index. If the move ends at the trap index,
+     * the number of steps is recalculated according to the trap value (new step = step - trap value).
+     * If the new step count is 0 or less than 0, no move is made. If it is greater than 0,
+     * move clockwise along the sides until the number of remaining step is 0.
+     */
 
     int is_trap;      // Default value is 0, it will be 1 if movement ended on a trap.
     int old_step;
@@ -387,27 +361,38 @@ int stepComputer(char board[BOARD_SIZE][BOARD_SIZE], int row, int col, int step,
     is_trap = checkIsOnTrap(step, p_row, p_col, t_row, t_col);
     if (is_trap == 1) {
         old_step = step;
-        step = step - t_val;
+        step = step - trap_val;
+
         if (step < 0)
             step = 0;
-        printf("\nTrapped at index %d %d! %d forward - %d backward = %d step.", t_row, t_col, old_step, t_val, step);
+
+        printf("\nTrapped at index %d %d! %d forward - %d backward = %d step.",
+               t_row, t_col, old_step, trap_val, step);
     }
 
-    // Move according to the direction value.
+    // Move the computer. If circle completed, return 1. Else 0.
+    return move(board, dir, step, row, col, p_row, p_col, COMPUTER);
+}
+
+
+// Move according to the direction value.
+int move(char board[BOARD_SIZE][BOARD_SIZE], int dir, int step, int row, int col, int p_row, int p_col, char player){
+    // Move clockwise along the sides until the number of remaining step is 0. If circle completed, return 1. Else 0.
+
     switch (dir) {
         case 0: // Move to the right. If necessary down.
             for (int i = 0; i < (col - 1); i++) {
                 if (step > 0 && p_col < (col - 1)) {
                     step--;
                     p_col++;
-                    board[0][p_col] = COMPUTER;
+                    board[0][p_col] = player;
                 }
             }
             for (int i = 0; i < (row - 1); i++) {
                 if (step > 0 && p_row < (row - 1)) {
                     step--;
                     p_row++;
-                    board[p_row][9] = COMPUTER;
+                    board[p_row][9] = player;
                 }
             }
             break;
@@ -416,8 +401,8 @@ int stepComputer(char board[BOARD_SIZE][BOARD_SIZE], int row, int col, int step,
                 if (step > 0 && p_row < row) {
                     step--;
                     p_row++;
-                    board[p_row][9] = COMPUTER;
-                    if (p_row == 9)
+                    board[p_row][9] = player;
+                    if (p_row == 9 && player == 'C')
                         return 1;
                 }
             }
@@ -427,14 +412,14 @@ int stepComputer(char board[BOARD_SIZE][BOARD_SIZE], int row, int col, int step,
                 if (step > 0 && p_col > 0) {
                     step--;
                     p_col--;
-                    board[9][p_col] = COMPUTER;
+                    board[9][p_col] = player;
                 }
             }
             for (int i = (row - 1); i > 0; i--) {
                 if (step > 0 && p_row > 0) {
                     step--;
                     p_row--;
-                    board[p_row][0] = COMPUTER;
+                    board[p_row][0] = player;
                 }
             }
             break;
@@ -443,14 +428,16 @@ int stepComputer(char board[BOARD_SIZE][BOARD_SIZE], int row, int col, int step,
                 if (step > 0 && p_row > 0) {
                     step--;
                     p_row--;
-                    board[p_row][0] = COMPUTER;
+                    board[p_row][0] = player;
+                    if(p_row == 0 && player == 'P')
+                        return 1;
                 }
             }
             for (int i = 0; i < (col - 1); i++) {
                 if (step > 0 && p_col < (col - 1)) {
                     step--;
                     p_col++;
-                    board[0][p_col] = COMPUTER;
+                    board[0][p_col] = player;
                 }
             }
     }
@@ -462,7 +449,6 @@ int stepComputer(char board[BOARD_SIZE][BOARD_SIZE], int row, int col, int step,
 // Check if the index is placed around the board.
 int checkTrapIndex(int row, int col) {
     // Return 1 if it is around the board, else return 0.
-
 
     if (row == 0 || row == 9) {
         if (col >= 0 && col <= 9)
@@ -499,9 +485,9 @@ void generateTrapIndex(int *row, int *col) {
 
 
 // Generate trap values for computer and player in range [1,5].
-void generateTrapValues(int *c_index, int *p_index) {
-    *c_index = 1 + rand() % 5; // Randomly generate a number between 1 and 5.
-    *p_index = 1 + rand() % 5;
+void generateTrapValues(int *c_trap_val, int *p_trap_val) {
+    *c_trap_val = 1 + rand() % 5; // Randomly generate a number between 1 and 5.
+    *p_trap_val = 1 + rand() % 5;
 }
 
 
@@ -518,9 +504,9 @@ void displayBoard(char board[BOARD_SIZE][BOARD_SIZE], int row, int col) {
 }
 
 
-// Should I explain this :D
+// Randomly generate a number between 1 and 6 for dice.
 int rollDice(void) {
-    return 1 + rand() % 6; // Randomly generate a number between 1 and 6.
+    return 1 + rand() % 6;
 }
 
 
